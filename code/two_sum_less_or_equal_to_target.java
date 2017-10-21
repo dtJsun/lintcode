705cdd8fd86f430e9a8b63c94898ebfaafeb49d3
Jsun <sun@JsundeMacBook-Pro.local>
2017-10-21 14:55:11 -0400

    new question

---
+++ code/two_sum_less_or_equal_to_target.java
@@ -0,0 +1,14 @@
+public class Solution {
+    /*
+     * @param nums: an array of integer
+     * @param target: an integer
+     * @return: an integer
+     */
+    public int twoSum5(int[] nums, int target) {
+        // write your code here
+
+        
+
+
+    }
+}
