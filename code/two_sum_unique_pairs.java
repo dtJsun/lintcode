c7c23d2507bb5d69a4c0aaa689a57e01d419dc20
Jsun <dtsunzhe@gmail.com>
2017-10-21 15:28:11 -0400

    added new draft

---
+++ code/two_sum_unique_pairs.java
@@ -0,0 +1,10 @@
+public class Solution {
+    /*
+     * @param nums: an array of integer
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int twoSum6(int[] nums, int target) {
+        // write your code here
+    }
+}
