96d35723e160b24818d6639ab1c06dcbe0304822
Jsun <sun@JsundeMacBook-Pro.local>
2017-10-21 15:20:43 -0400

    AC question

--- code/two_sum_less_or_equal_to_target.java
+++ code/two_sum_less_or_equal_to_target.java
@@ -7,8 +7,22 @@ public class Solution {
     public int twoSum5(int[] nums, int target) {
         // write your code here
 
-        
+        int start = 0;
+        int end = nums.length - 1;
+        int count = 0;
 
+        Arrays.sort(nums);
+        while (start < end) {
+            int sum = nums[start] + nums[end];
+            if (sum > target) {
+                end--;
+            } else {
+                count += end - start;
+                start++;
+            }
+        }
+
+        return count;
 
     }
 }
