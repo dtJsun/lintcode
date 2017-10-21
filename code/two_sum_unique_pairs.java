2e20d82061166bd55bc0a0e79a4a5fa3c0f35c74
Jsun <dtsunzhe@gmail.com>
2017-10-21 15:52:31 -0400

    AC

--- code/two_sum_unique_pairs.java
+++ code/two_sum_unique_pairs.java
@@ -6,5 +6,26 @@ public class Solution {
      */
     public int twoSum6(int[] nums, int target) {
         // write your code here
+
+        HashSet<String> set = new HashSet<>();
+
+        Arrays.sort(nums);
+
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start < end) {
+            int sum = nums[start] + nums[end];
+            if (sum < target) {
+                start++;
+            } else if (sum > target) {
+                end--;
+            } else {
+                set.add(Integer.toString(nums[start]) + Integer.toString(nums[end]));
+                start++;
+                end--;
+            }
+        }
+        return set.size();
     }
 }
