a6dd45ddfd46c8827d89d31c4fdc67eabd0fa056
Jsun <dtsunzhe@gmail.com>
2017-10-24 23:11:43 -0400

    AC quick sort

--- code/sort_colors_ii.java
+++ code/sort_colors_ii.java
@@ -6,8 +6,34 @@ public class Solution {
      */
     public void sortColors2(int[] colors, int k) {
         // write your code here
+        quickSort(colors, 0, colors.length - 1);
+    }
 
+    private void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
 
-        
+    private int partition(int[] arr, int low, int high) {
+
+        pivot = arr[high];
+        int i = low - 1;
+        for (j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                swap(arry, i, j);
+            }
+        }
+        swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
     }
 }
