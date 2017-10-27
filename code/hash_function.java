617fdf8caf90a10c6814057fbe7e7e5d3b238a34
Jsun <dtsunzhe@gmail.com>
2017-10-26 21:53:20 -0400

    AC and new questions

--- code/hash_function.java
+++ code/hash_function.java
@@ -7,9 +7,17 @@ public class Solution {
     public int hashCode(char[] key, int HASH_SIZE) {
         // write your code here
         
-        int len = key.length;
+        int fac = key.length - 1;
 
-        
+        int sum = 0;
+        int magic = 33 % HASH_SIZE;
+
+        for (int i = 0; i <= fac; i++) {
+            int temp = (int)key[i];
+            int 
+
+
+        }
 
         
     }
---
+++ code/high_five.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a Record
+ * class Record {
+ *     public int id, score;
+ *     public Record(int id, int score){
+ *         this.id = id;
+ *         this.score = score;
+ *     }
+ * }
+ */
+public class Solution {
+    /**
+     * @param results a list of <student_id, score>
+     * @return find the average of 5 highest scores for each person
+     * Map<Integer, Double> (student_id, average_score)
+     */
+    public Map<Integer, Double> highFive(Record[] results) {
+        // Write your code here
+    }
+}
