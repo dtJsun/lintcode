ec437c2289d463d55a24a3857c52796bd424c0d3
Jsun <dtsunzhe@gmail.com>
2017-11-04 19:19:49 -0400

    AC

---
+++ code/heapify.java
@@ -0,0 +1,13 @@
+/**
+ * no more than sort an array :/
+ */
+public class Solution {
+    /*
+     * @param A: Given an integer array
+     * @return: nothing
+     */
+    public void heapify(int[] A) {
+        // write your code here
+        Arrays.sort(A);
+    }
+}
