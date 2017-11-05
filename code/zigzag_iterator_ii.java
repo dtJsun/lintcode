dc41817e52c892f1fd402e1aef62aaca61a83284
Jsun <dtsunzhe@gmail.com>
2017-11-05 15:59:05 -0500

    AC

--- code/zigzag_iterator_ii.java
+++ code/zigzag_iterator_ii.java
@@ -1,8 +1,28 @@
 public class ZigzagIterator2 {
+
+    class Element {
+        int row;
+        int col;
+        public Element(int r, int c) {
+            row = r;
+            col = c;
+        }
+    }
+
+    private Element element;
+    private List<List<Integer>> list;
+
     /*
     * @param vecs: a list of 1d vectors
     */public ZigzagIterator2(List<List<Integer>> vecs) {
         // do intialization if necessary
+        list = new ArrayList<>();
+        for (int i = 0; i < vecs.size(); i++) {
+            if (vecs.get(i).size() > 0) {
+                list.add(new ArrayList<Integer>(vecs.get(i)));
+            }
+        }
+        element = new Element(-1, -1);
     }
 
     /*
@@ -10,6 +30,7 @@ public class ZigzagIterator2 {
      */
     public int next() {
         // write your code here
+        return list.get(element.row).get(element.col);
     }
 
     /*
@@ -17,6 +38,18 @@ public class ZigzagIterator2 {
      */
     public boolean hasNext() {
         // write your code here
+        int count = 0;
+        while (count < list.size()) {
+            element.row = (element.row + 1) % list.size();
+            if (element.row == 0) {
+                element.col += 1;
+            }
+            if (element.col < list.get(element.row).size()) {
+                return true;
+            }
+            count++;
+        }
+        return false;
     }
 }
 
