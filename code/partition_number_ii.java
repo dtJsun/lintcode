5697d170c908d8313279da370726ec5c30a81491
Jsun <dtsunzhe@gmail.com>
2017-11-11 15:16:45 -0500

    AC

--- code/partition_number_ii.java
+++ code/partition_number_ii.java
@@ -8,8 +8,29 @@ public class Solution {
     public void partition2(int[] nums, int low, int high) {
         // write your code here
         
+        int lowPtr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < low) {
+                swap(nums, lowPtr, i);
+                lowPtr++;
+            }
+        }
 
+        int highPtr = nums.length - 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] > high) {
+                swap(nums, highPtr, i);
+                highPtr--;
+            }
+        }
 
+        return;
 
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
