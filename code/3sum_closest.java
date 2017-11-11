20795017462d98e011e0ca087c9c2f317eaee189
Jsun <dtsunzhe@gmail.com>
2017-11-11 18:21:48 -0500

    AC and new question

--- code/3sum_closest.java
+++ code/3sum_closest.java
@@ -7,12 +7,30 @@ public class Solution {
     public int threeSumClosest(int[] numbers, int target) {
         // write your code here
 
-        
-
+        Arrays.sort(numbers);
+        int diff = Integer.MAX_VALUE;
+        int ans = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            rest = target - numbers[i];
+            int start = i + 1;
+            int end = numbers.length - 1;
+            while (start < end) {
+                int sum = numbers[start] + numbers[end];
+                int tempDiff = Math.abs(rest - sum);
+                if (tempDiff < diff) {
+                    diff = tempDiff;
+                    ans = sum + numbers[i];
+                    if (tempDiff == 0) return ans;
+                }
+                if (sum > rest) {
+                    end--;
+                } else {
+                    start++;
+                }
+            }
+        }
+        return ans;
 
     }
-
-
-
 
 }
