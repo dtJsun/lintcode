a98da3fbb062f02f85153da0bce2882cc44b620a
Jsun <dtsunzhe@gmail.com>
2017-11-11 19:44:01 -0500

    AC

--- code/intersection_of_two_arrays_ii.java
+++ code/intersection_of_two_arrays_ii.java
@@ -8,25 +8,27 @@ public class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         // write your code here
 
-        HashSet<Integer> set = new HashSet<>();
-        HashSet<Integer> temp = new HashSet<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums1.length; i++) {
-            set.add(nums1[i]);
+            if (!map.containsKey(nums1[i])) {
+                map.put(nums1[i], 1);
+            } else {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            }
         }
+
+        List<Integer> temp = new ArrayList<>();
         for (int i = 0; i < nums2.length; i++) {
-            if (set.contains(nums2[i])) {
+            if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
                 temp.add(nums2[i]);
+                map.put(nums2[i], map.get(nums2[i]) - 1);
             }
         }
         int[] ans = new int[temp.size()];
-        int index = 0;
-        for (Integer i : temp) {
-            ans[index] = i;
-            index++;
+        for (int i = 0; i < temp.size(); i++) {
+            ans[i] = temp.get(i);
         }
-
         return ans;
-
     }
 };
