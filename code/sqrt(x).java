08b969669426bee2c5e2710b279ed3d94d70c0dd
Jsun <dtsunzhe@gmail.com>
2017-11-21 21:53:20 -0500

    AC

---
+++ code/sqrt(x).java
@@ -0,0 +1,25 @@
+public class Solution {
+    /*
+     * @param x: An integer
+     * @return: The sqrt of x
+     */
+    public int sqrt(int x) {
+        // write your code here
+        long start = 0, end = x;
+
+        while (start + 1 < end) {
+            long mid = start + (end - start) / 2;
+
+            if (mid * mid == x) {
+                return (int)mid;
+            } else if (mid * mid < x) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (end * end <= x) return (int)end;
+        return (int)start;
+    }
+}
