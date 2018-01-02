539d5b5cbda5edad724073b8eeb43903f940fd5f
Jsun <dtsunzhe@gmail.com>
2018-01-02 16:27:07 +0800

    min stack

--- code/medium1/min_stack.cpp
+++ code/medium1/min_stack.cpp
@@ -1,27 +1,44 @@
+// pop 之后不要忘记更新minValue的值
 #include <iostream>
+#include <stack>
+#include <climits>
 using namespace std;
 
 class MinStack {
+
+private:
+    stack<int> stk;
+    stack<int> minStk;
+    int minValue;
+
 public:
     /** initialize your data structure here. */
     MinStack() {
-        
+        minValue = INT_MAX;
     }
     
     void push(int x) {
-        
+        stk.push(x);
+        minValue = min(x, minValue);
+        minStk.push(minValue);        
     }
     
     void pop() {
-        
+        stk.pop();
+        minStk.pop();
+        if (stk.empty()) {
+            minValue = INT_MAX;
+            return;
+        }
+        minValue = minStk.top();
     }
     
     int top() {
-        
+        return stk.top();
     }
     
     int getMin() {
-        
+        return minStk.top();
     }
 };
 
