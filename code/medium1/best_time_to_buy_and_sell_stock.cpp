966c95c9a23ebe4fb66086ff660a62e7a62c957d
Jsun <dtsunzhe@gmail.com>
2018-01-04 10:49:59 +0800

    best time to buy and sell stock

--- code/medium1/best_time_to_buy_and_sell_stock.cpp
+++ code/medium1/best_time_to_buy_and_sell_stock.cpp
@@ -1,11 +1,31 @@
+// 先求出每两天之间的差值，再用maximum subarray 做
+// 注意maximum subarray 中的for循环是从i = 1 开始的
 #include <vector>
-#include <iostream>
 using namespace std;
 
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         
+        if (prices.size() <= 1) return 0;
+        
+        vector<int> diffs(prices.size() - 1, 0);
+        
+        for (int i = diffs.size(); i > 0; i--) {
+            diffs[i - 1] = prices[i] - prices[i - 1];
+        }
+        
+        int sum = diffs[0];
+        int maxSum = diffs[0];
+        for (int i = 1; i < diffs.size(); i++) {
+            if (sum < 0) {
+                sum = diffs[i];
+            } else {
+                sum += diffs[i];
+            }
+            maxSum = max(sum, maxSum);
+        }
+        return max(0, maxSum);
+        
     }
 };
-// solution 1: O(n2) calculate the differences one by one
